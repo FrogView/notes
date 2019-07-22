6b00c30292f106070d0596134dc8cec79e3ec661
FrogView <jhongbo@163.com>
2019-07-22 13:36:18 +0800

    CosmosDB Helper

---
+++ .Net/CosmosDBHelper.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using Microsoft.Azure.Documents.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CosmosDBFunc
+{
+    public class CosmosDBHelper<T>
+    {
+        private static readonly string Endpoint = "";
+        private static readonly string Key = "";
+        
+        private string DatabaseId = "Results";
+        private string CollectionId = "";//RunResults,UtteranceResults
+        public DocumentClient DbClient { get; set; }
+        public CosmosDBHelper(string databaseId,string collectionId)
+        {
+            this.DatabaseId = databaseId;
+            this.CollectionId = collectionId;
+
+            DbClient = new DocumentClient(new Uri(Endpoint), Key);
+        }
+
+        public async Task InitDB()
+        {
+            await DbClient.CreateDatabaseIfNotExistsAsync(new Database { Id = this.DatabaseId });
+            await DbClient.CreateDocumentCollectionIfNotExistsAsync(UriFactory.CreateDatabaseUri(DatabaseId), new DocumentCollection { Id = CollectionId });
+
+        }
+
+
+        public async Task<T> GetItemAsync(string id)
+        {
+            try
+            {
+                Document document = await DbClient.ReadDocumentAsync(UriFactory.CreateDocumentUri(DatabaseId, CollectionId, id));
+                return (T)(dynamic)document;
+            }
+            catch (DocumentClientException e)
+            {
+                if (e.StatusCode == System.Net.HttpStatusCode.NotFound)
+                {
+                    return default(T);
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+
+        public async Task<IEnumerable<T>> GetItemsAsync(Expression<Func<T, bool>> predicate)
+        {
+            IDocumentQuery<T> query = DbClient.CreateDocumentQuery<T>(
+                UriFactory.CreateDocumentCollectionUri(DatabaseId, CollectionId),
+                new FeedOptions { MaxItemCount = -1, EnableCrossPartitionQuery = true })
+                .Where(predicate)
+                .AsDocumentQuery();
+
+            List<T> results = new List<T>();
+            while (query.HasMoreResults)
+            {
+                results.AddRange(await query.ExecuteNextAsync<T>());
+            }
+
+            return results;
+        }
+
+        public async Task<IEnumerable<T>> GetItemsAsync(string sqlExpression)
+        {
+            var query = DbClient.CreateDocumentQuery(UriFactory.CreateDocumentCollectionUri(DatabaseId, CollectionId),
+           sqlExpression, new FeedOptions { MaxItemCount = -1 , EnableCrossPartitionQuery = true }).AsDocumentQuery();
+
+            List<T> results = new List<T>();
+            while (query.HasMoreResults)
+            {
+                results.AddRange(await query.ExecuteNextAsync<T>());
+            }
+
+            return results;
+        }
+
+        public async Task<Document> CreateItemAsync(T item)
+        {
+            return await DbClient.CreateDocumentAsync(UriFactory.CreateDocumentCollectionUri(DatabaseId, CollectionId), item);
+        }
+
+        public async Task<Document> UpdateItemAsync(string id, T item)
+        {
+            return await DbClient.ReplaceDocumentAsync(UriFactory.CreateDocumentUri(DatabaseId, CollectionId, id), item);
+        }
+
+        public async Task DeleteItemAsync(string id)
+        {
+            await DbClient.DeleteDocumentAsync(UriFactory.CreateDocumentUri(DatabaseId, CollectionId, id));
+        }
+
+    }
+}
